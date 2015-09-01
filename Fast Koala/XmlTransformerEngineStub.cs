ebc83d3309a6bff291ad369465f68032165691b8
Jon Davis <jon@jondavis.net>
2015-09-01 00:49:36 -0700

    Update log output for PS and JS; eliminate unnecessary "app.config has changed" alerts (use TransformXml generated output to compare changes to the file, ignore if unchanged)

---
+++ Fast Koala/XmlTransformerEngineStub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace Wijits.FastKoala
+{
+    public class XmlTransformerEngineStub : IBuildEngine
+    {
+        public void LogErrorEvent(BuildErrorEventArgs e)
+        {
+        }
+
+        public void LogWarningEvent(BuildWarningEventArgs e)
+        {
+        }
+
+        public void LogMessageEvent(BuildMessageEventArgs e)
+        {
+        }
+
+        public void LogCustomEvent(CustomBuildEventArgs e)
+        {
+        }
+
+        public bool BuildProjectFile(string projectFileName, string[] targetNames, IDictionary globalProperties,
+            IDictionary targetOutputs)
+        {
+            return false;
+        }
+
+        public bool ContinueOnError { get; private set; }
+        public int LineNumberOfTaskNode { get; private set; }
+        public int ColumnNumberOfTaskNode { get; private set; }
+        public string ProjectFileOfTaskNode { get; private set; }
+    }
+}
