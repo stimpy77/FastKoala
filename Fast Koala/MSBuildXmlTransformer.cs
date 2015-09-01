e9a7e571617e2232e5c21f9a322fac9feb998afe
Jon Davis <jon@jondavis.net>
2015-09-01 00:49:03 -0700

    Update log output for PS and JS; eliminate unnecessary "app.config has changed" alerts (use TransformXml generated output to compare changes to the file, ignore if unchanged)

---
+++ Fast Koala/MSBuildXmlTransformer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Wijits.FastKoala
+{
+    class MSBuildXmlTransformer
+    {
+        private Microsoft.Web.Publishing.Tasks.TransformXml _xfrm;
+        public MSBuildXmlTransformer()
+        {
+            _xfrm = new Microsoft.Web.Publishing.Tasks.TransformXml
+            {
+                BuildEngine = new XmlTransformerEngineStub()
+            };
+        }
+
+        public string Source
+        {
+            get { return _xfrm.Source; }
+            set { _xfrm.Source = value; }
+        }
+
+        public string Transform
+        {
+            get { return _xfrm.Transform; }
+            set { _xfrm.Transform = value; }
+        }
+
+        public string Destination
+        {
+            get { return _xfrm.Destination; }
+            set { _xfrm.Destination = value; }
+        }
+
+        public bool Execute()
+        {
+            return _xfrm.Execute();
+        }
+    }
+}
